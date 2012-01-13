1f3c001df573698a7abec9a25b08df4ff6de9328
Tina Lichtenthaeler <lichtenc@in.tum.de>
2012-01-13 13:20:20 +0000

    Added missing initialization and fixed wrong odometry transformation

--- rflex/src/b21_node.cc
+++ rflex/src/b21_node.cc
@@ -193,6 +193,8 @@ void B21Node::publishOdometry() {
     if (!initialized) {
         initialized = true;
         first_bearing = true_bearing;
+	x_odo = 0;
+	y_odo = 0;
         a_odo = 0*true_bearing;
     } else {
         float bearing = true_bearing - first_bearing;
@@ -213,7 +215,7 @@ void B21Node::publishOdometry() {
     last_bearing = true_bearing - first_bearing;
 
     //since all odometry is 6DOF we'll need a quaternion created from yaw
-    geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(-first_bearing);
+    geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(last_bearing);
 
     //first, we'll publish the transform over tf
     geometry_msgs::TransformStamped odom_trans;
